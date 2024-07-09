57391c5c3ff5692df901677472985c941c541048
Samuel Castaneda <samuelcda13@gmail.com>
2024-07-09 17:09:11 +0000

    Correct typescript-arrays Exercise

--- typescript-arrays/main.js
+++ typescript-arrays/main.js
@@ -3,12 +3,10 @@ const colors = ['red', 'white', 'blue'];
 console.log('Value of color[0]: ', colors[0]);
 console.log('Value of color[1]: ', colors[1]);
 console.log('Value of color[2]: ', colors[2]);
-const america = `America is ${colors[0]}, ${colors[2]},
-                      and ${colors[2]}.`;
+const america = `America is ${colors[0]}, ${colors[1]}, and ${colors[2]}.`;
 console.log(america);
 colors[2] = 'green';
-const Mexico = `Mexico is  ${colors[0]}, ${colors[2]},
-                      and ${colors[2]}.`;
+const Mexico = `Mexico is  ${colors[0]}, ${colors[1]}, and ${colors[2]}.`;
 console.log(Mexico);
 console.log('The values of colors: ', colors);
 console.log('The type of colors: ', typeof colors);
