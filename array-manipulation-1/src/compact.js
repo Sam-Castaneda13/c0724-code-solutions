43169042d096b4dcb8a99b36c20b12ccecae7654
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-07-11 23:15:45 +0000

    Completed array-manipulation-1 Exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,2 +1,12 @@
 'use strict';
 /* exported compact */
+function compact(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const checker = Boolean(array[i]);
+    if (checker !== false) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-1/src/compact.ts
+++ array-manipulation-1/src/compact.ts
@@ -1,1 +1,12 @@
 /* exported compact */
+function compact(array: unknown[]): unknown[] {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const checker = Boolean(array[i]);
+
+    if (checker !== false) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
