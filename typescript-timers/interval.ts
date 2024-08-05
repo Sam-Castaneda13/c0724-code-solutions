40855f6d1a74c61b56fc87be5bdb62826455289e
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-08-05 18:10:08 +0000

    completed typescript-timers exercise

---
+++ typescript-timers/interval.ts
@@ -0,0 +1,22 @@
+const $timer = document.querySelector('.countdown-display');
+if (!$timer)
+  throw new Error('The class countdown-display did not load correctly');
+
+let count = 4;
+const intervalId = setInterval(function () {
+  count--;
+  switch (count) {
+    case 3:
+      $timer.textContent = '3';
+      break;
+    case 2:
+      $timer.textContent = '2';
+      break;
+    case 1:
+      $timer.textContent = '1';
+      break;
+    case 0:
+      $timer.textContent = '~Earth Beeeelooowww Us~';
+      clearInterval(intervalId);
+  }
+}, 2000);
