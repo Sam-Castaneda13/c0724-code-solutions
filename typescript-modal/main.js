6a5aa4f3ed938ed5b32267f292ff10f25154106c
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-08-06 23:09:43 +0000

    Ill be back again part 2

--- typescript-modal/main.js
+++ typescript-modal/main.js
@@ -1,1 +1,10 @@
 'use strict';
+const $modal = document.querySelector('dialog');
+if (!$modal) throw new Error('It could not load dialog method');
+const $open = document.querySelector('.open-modal');
+if (!$open) throw new Error('The open button could not load');
+const $dismiss = document.querySelector('.dismiss-modal');
+if (!$dismiss) throw new Error('It could not load the dismiss button');
+$open.addEventListener('click', function () {
+  $modal.show();
+});
